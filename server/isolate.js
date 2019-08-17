270ef8298b537492787c8c3b52379e76eda7b4b1
Robert Chen <robertchen@live.com>
2019-08-17 15:00:04 +0800

    Improved validation of isolate options

--- server/isolate.js
+++ server/isolate.js
@@ -1,4 +1,5 @@
 const config = require(__rootdir + "/config.json");
+const Joi = require("@hapi/joi");
 
 const fs = require("fs");
 const path = require("path");
@@ -12,10 +13,38 @@ let lim = 0;
 
 const isolateCmd = "isolate --cg";
 
+const compileDefaults = {
+  time: 1,
+  wallTime: 10,
+  mem: 10 * 1000
+}
+
+const runDefaults = {
+  time: 4,
+  wallTime: 40,
+  mem: 100 * 1000,
+}
+
+const optToFlag = {
+  time: "--time",
+  wallTime: "--wall-time",
+  mem: "--cg-mem",
+  inFile: "--stdin"
+}
+
 class Isolate {
   static get langs() {
     return Object.keys(config.langs);
   }
+
+  static get schema() {
+    return Joi.object().keys({
+      time: Joi.number().min(0).integer(),
+      wallTime: Joi.number().min(0).integer(),
+      mem: Joi.number().min(0).integer(),
+    }).unknown(false);
+  }
+
   constructor(lang){
     if(!Isolate.langs.includes(lang)){
       throw `Invalid language ${lang}`;
@@ -36,21 +65,39 @@ class Isolate {
       this.rootPath = `${__rootdir}/tmp/${boxId}`
       fs.mkdirSync(this.rootPath, { recursive: true });
     } else {
-      const resp = execSync(`${isolateCmd} --init ${boxId}`);
+      const resp = execSync(`${isolateCmd} --init -b ${boxId}`);
 
       if(resp.includes("Sandbox ID out of range")) throw "Out of isolate boxes";
 
       this.rootPath = path.resolve(resp, "box");
     }
+
+    this.isolateCmd = `${isolateCmd} -b ${boxId}`
+  }
+
+  getCommandBase(opts) {
+    let cmd = "";
+    if(!TESTING) {
+      cmd = `${this.isolateCmd} -e`;
+      cmd = `${cmd} ${
+        Object.keys(opts).map(
+          key => `${optToFlag[key]} ${opts[key]}`
+        ).join(" ")
+      }`;
+      cmd = `${cmd} --run --`;
+    }
+    return cmd;
   }
 
   compile(source, opts, cb) {
-    if(!this.config.compile) return cb();
-
-    let cmd = "";
-    if(!TESTING) {
-      cmd = `${isolateCmd} -e --run --`;
+    if(!this.config.compile) {
+      this.compiledFile = source;
+      return cb();
     }
+    this.compiledFile = this.config.out; 
+
+    opts = Object.assign(compileDefaults, opts);
+    let cmd = this.getCommandBase(opts);
 
     const compileCmd = this.config.compile.cmd;
     const compileOpts = this.config.compile.opts;
@@ -62,30 +109,22 @@ class Isolate {
   }
 
   run(inFile, opts, cb) {
-    let cmd = "";
-    if(!TESTING) {
-      cmd = `${isolateCmd} -e --run --`;
-    }
+    opts = Object.assign(runDefaults, opts, {inFile});
+    let cmd = this.runCmd(opts);
 
-    if(this.config.run) {
-      
-    } else {
-      cmd = `${cmd} ${TESTING? this.rootPath + "/" : ""}${this.config.out}`;
-    }
+    if(TESTING) cmd = `cat ${inFile} | ${cmd}`;
 
     exec(cmd, cb);
   }
 
   runCmd(opts) {
-    let cmd = "";
-    if(!TESTING) {
-      cmd = `${isolateCmd} -e --run --`;
-    }
+    let cmd = this.getCommandBase(opts);
 
     if(this.config.run) {
-      
+      const compileCmd = this.config.compile.cmd;
+      const compileOpts = this.config.compile.opts;
     } else {
-      cmd = `${cmd} ${TESTING? this.rootPath + "/" : ""}${this.config.out}`;
+      cmd = `${cmd} ${TESTING? this.rootPath + "/" : ""}${this.compiledFile}`;
     }
 
     return cmd;
@@ -121,6 +160,10 @@ class Isolate {
 
   destroy() {
     cache.push(this.boxId);
+
+    if(!TESTING) {
+      execSync(`${this.isolateCmd} --destroy`);
+    }
   }
 
 
--- server/run.js
+++ server/run.js
@@ -8,94 +8,101 @@ const fs = require("fs");
 
 const Isolate = require(__rootdir + "/server/isolate.js");
 
+const isolateSchema = Isolate.schema;
+
 const apiSchema = Joi.object().keys({
-  lang: Joi.string().required(),
+  lang: Joi.string().valid(Isolate.langs).required(),
   source: Joi.string().required(),
-  filename: Joi.string().regex(/[a-zA-Z0-9\.]*/).max(30).required(),
+  filename: Joi.string().regex(/^[a-zA-Z0-9\.]*$/).max(30).required(),
   testsuite: Joi.string().alphanum().max(30).default("global"),
   grader: Joi.string().valid(config.graders),
   tests: Joi.array(),
-  compile: Joi.any(),
-  execute: Joi.any(),
+  compile: isolateSchema,
+  execute: isolateSchema,
 }).without("testsuite", "tests");
 
 router.post("/", (req, res) => {
   apiSchema.validate(req.body, (err, data) => {
-    const {lang, source, filename, testsuite, grader} = data;
-    if(Isolate.langs.includes(lang)) {
-      const box = new Isolate(lang);
-
-      fs.writeFileSync(`${box.rootPath}/${filename}`, source);
-
-      box.compile(filename, {}, (err, stdout, stderr) => {
-        if(err) {
-          return res.send({
-            err: "Compilation error",
-            stderr: stderr,
-            stdout: stdout
-          });
-        }
-
-        if(testsuite) {
-          const testsuiteDir = getTestDir(testsuite);
-          fs.readdir(testsuiteDir, (err, files) => {
-            if(err) {
-              return res.status(400).send(`Invalid testsuite: ${testsuite}`);
-            }
-
-            const testcases = files.filter(file => file.endsWith(config.inExt));
-            const results = [];
-
-            const finish = () => {
-              box.destroy();
-              res.send(results);
-            }
-            const runTest = () => {
-              let i = results.length;
-
-              if(i === testcases.length) return finish();
-              
-              const name = testcases[i].substring(0, testcases[i].length - config.inExt.length);
-
-              if(grader) {
-                const outputFile = name + config.outExt;
-
-                box.runGrader(`${testsuiteDir}/${testcases[i]}`, `${testsuiteDir}/${outputFile}`, getGraderDir(grader), {}, (err, result) => {
-                  results.push({
-                    name,
-                    result,
-                    err
-                  });
-
-                  runTest();
-                })
-              } else {
-                const inputFileName = "data.in";
-                fs.copyFile(`${testsuiteDir}/${testcases[i]}`, `${box.rootPath}/${inputFileName}`, err => {
-                  if(err) return res.status(500).send("Error when copying input files");
-
-                  box.run(inputFileName, {}, (err, stdout, stderr) => {
-                    results.push({
-                      name,
-                      err,
-                      stdout,
-                      stderr
-                    });
-                    runTest();
-                  });
-                });
-              }
-            }
-
-            runTest();
-          });
-        }
-      });      
-    } else {
+    if(err) {
       return res.status(400).send({
-        err: "Invalid language. Supported langs: [" + Isolate.langs.join(" ") + "]"
+        err: "Parameter validation failed",
+        details: err.details.map(err => err.message)
       });
     }
+    const {lang, source, filename, testsuite, grader, tests} = data;
+    const box = new Isolate(lang);
+
+    const runAllTests = () => {
+      const testsuiteDir = getTestDir(testsuite);
+      fs.readdir(testsuiteDir, (err, files) => {
+        if(err) {
+          return res.status(400).send(`Invalid testsuite: ${testsuite}`);
+        }
+
+        const getTestName = inFileName => inFileName.substring(0, inFileName.length - config.inExt.length);
+
+        const testcases = files
+          .filter(file => file.endsWith(config.inExt))
+          .filter(file => {
+            if(!tests) return true;
+            return tests.includes(getTestName(file));
+          });
+
+        const results = [];
+
+        const finish = () => {
+          box.destroy();
+          res.send(results);
+        }
+
+        const runTest = () => {
+          let i = results.length;
+
+          if(i === testcases.length) return finish();
+          
+          const name = getTestName(testcases[i])
+          
+          if(grader) {
+            const outputFile = name + config.outExt;
+
+            box.runGrader(`${testsuiteDir}/${testcases[i]}`, `${testsuiteDir}/${outputFile}`, getGraderDir(grader), {}, (err, result) => {
+              results.push({
+                name,
+                result,
+                err
+              });
+
+              runTest();
+            })
+          } else {
+            box.run(`${testsuiteDir}/${testcases[i]}`, {}, (err, stdout, stderr) => {
+              results.push({
+                name,
+                err,
+                stdout,
+                stderr
+              });
+              runTest();
+            });
+          }
+        }
+
+        runTest();
+      });
+    }
+
+    fs.writeFileSync(`${box.rootPath}/${filename}`, source);
+    box.compile(filename, {}, (err, stdout, stderr) => {
+      if(err) {
+        return res.send({
+          err: "Compilation error",
+          stderr: stderr,
+          stdout: stdout
+        });
+      }
+
+      runAllTests();
+    });      
   });
 });
 
